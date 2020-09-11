60a5f548fdd1edc56e0e4352d9d3fcc456be747d
ProdigyX6217 <dominic.holder.15@cityas.org>
2020-09-11 00:14:33 -0600

    Load Game Stage

---
+++ The_Ruins/GameView.swift
@@ -0,0 +1,40 @@
+//
+//  GameView.swift
+//  The_Ruins
+//
+//  Created by Student Laptop_7/19_1 on 9/10/20.
+//  Copyright © 2020 Makeschool. All rights reserved.
+//
+
+import SpriteKit
+import SceneKit
+
+// will hold the SpriteKit 2D UI
+
+class GameView: SCNView {
+    
+    //    MARK:- lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    deinit {
+        
+    }
+    
+    
+//    MARK:- internal functions
+    
+//    MARK:- D-Pad
+    
+//    MARK:- attack button
+
+    
+    
+}
--- The_Ruins/GameViewController.swift
+++ The_Ruins/GameViewController.swift
@@ -7,108 +7,34 @@
 //
 
 import UIKit
-import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+enum GameState {
+    case loading, playing
+}
 
+class GameViewController: UIViewController {
+    
+//    scene
+    var gameView:GameView {return view as! GameView }
+    var mainScene: SCNScene!
+    
+//    general
+    var gameState:GameState = .loading
+
+    
+//    MARK:- lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        setupScene()
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        gameState = .playing
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
+    override var shouldAutorotate: Bool { return true }
     
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var prefersStatusBarHidden: Bool { return true }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
@@ -118,4 +44,32 @@ class GameViewController: UIViewController {
         }
     }
 
+    
+//    MARK:- scene
+    private func setupScene() {
+        
+        gameView.allowsCameraControl = true
+        gameView.antialiasingMode = .multisampling4X
+        
+        mainScene = SCNScene(named: "art.scnassets/Scenes/Stage1.scn")
+        gameView.scene = mainScene
+        gameView.isPlaying = true
+        
+    }
+    
+    
+//    MARK:- walls
+    
+//    MARK:- camera
+    
+//    MARK:- player
+
+//    MARK:- touches + movement
+
+//    MARK:- game loop functions
+    
+//    MARK:- enemies
+    
 }
+
+//    MARK:- extensions
